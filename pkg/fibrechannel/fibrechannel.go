3b18ce2d74c9a1b0ad898aa04e76cfc67e1f7e5a
Yussuf Shaikh <yussuf.shaikh1@ibm.com>
2023-04-05 14:12:26 +0530

    add wait for both findDisk funcs
    Signed-off-by: Yussuf Shaikh <yussuf.shaikh1@ibm.com>

--- pkg/fibrechannel/fibrechannel.go
+++ pkg/fibrechannel/fibrechannel.go
@@ -152,10 +152,33 @@ func searchDisk(c Connector, io ioHandler) (string, error) {
 	for {
 
 		for _, diskID := range diskIds {
-			if len(c.TargetWWNs) != 0 {
-				disk, dm = findDisk(diskID, c.Lun, io)
+			if !rescaned {
+				if len(c.TargetWWNs) != 0 {
+					disk, dm = findDisk(diskID, c.Lun, io)
+				} else {
+					disk, dm = findDiskWWIDs(diskID, io)
+				}
 			} else {
-				disk, dm = pollAndFindDiskWWIDs(diskID, io)
+				err := wait.PollImmediate(3*time.Second, 5*time.Minute, func() (bool, error) {
+					if len(c.TargetWWNs) != 0 {
+						disk, dm = findDisk(diskID, c.Lun, io)
+					} else {
+						disk, dm = findDiskWWIDs(diskID, io)
+					}
+					// if no disk matches then retry
+					if disk == "" && dm == "" {
+						return false, fmt.Errorf("no fc disk found")
+					}
+					// found the disk before timeout
+					if disk != "" {
+						return true, nil
+					}
+					// wait until timeout
+					return false, nil
+				})
+				if err == nil && disk == "" && dm == "" {
+					klog.Errorf("failed within timeout")
+				}
 			}
 			// if multipath device is found, break
 			if dm != "" {
@@ -188,26 +211,6 @@ func searchDisk(c Connector, io ioHandler) (string, error) {
 	}
 
 	return disk, nil
-}
-func pollAndFindDiskWWIDs(wwid string, io ioHandler) (string, string) {
-	disk, dm := "", ""
-	err := wait.PollImmediate(3*time.Second, 5*time.Minute, func() (bool, error) {
-		disk, dm = findDiskWWIDs(wwid, io)
-		// if no disk matches then retry
-		if disk == "" && dm == "" {
-			return false, fmt.Errorf("no fc disk found")
-		}
-		// found the disk before timeout
-		if disk != "" {
-			return true, nil
-		}
-		// wait until timeout
-		return false, nil
-	})
-	if err == nil && disk == "" && dm == "" {
-		klog.Errorf("failed within timeout")
-	}
-	return disk, dm
 }
 
 // given a wwn and lun, find the device and associated devicemapper parent
